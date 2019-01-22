83cbd88928de731b900deba17c2cb4f4872927ff
deltagare <deltagare@LEXJONKAF8>
2019-01-22 09:19:40 +0100

    Add project files.

---
+++ Range of Data types/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Range_of_Data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
